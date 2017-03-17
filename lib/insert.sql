3e116af36d584aab75bb70f00e25a2c7b27e89f8
Ahmad Abdel-Halim <ahmadabdelhalimmoustafa@gmail.com>
2017-03-17 20:17:09 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name) VALUES ("Mr. Chocolate");
+INSERT INTO bears (name) VALUES ("Rowdy");
+INSERT INTO bears (name) VALUES ("Tabitha");
+INSERT INTO bears (name) VALUES ("Sergeant Brown");
+INSERT INTO bears (name) VALUES ("Melissa");
+INSERT INTO bears (name) VALUES ("Grinch");
+INSERT INTO bears (name) VALUES ("Wendy");
+INSERT INTO bears (name) VALUES (null);
