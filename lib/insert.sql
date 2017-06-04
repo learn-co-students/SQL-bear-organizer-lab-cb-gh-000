cd6cedd3c53de4b8bbbf1936e8cf6c491e3644ea
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2017-06-04 05:48:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Mr. Chocolate', 20, 'M', 'brown', 'happy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Rowdy', 35, 'M', 'brown', 'happy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Tabitha', 40, 'F', 'brown', 'happy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sergeant Brown', 30, 'M', 'brown', 'happy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Melissa', 28, 'F', 'pink', 'happy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Grinch', 25, 'M', 'orange', 'confused', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Wendy', 18, 'F', 'purple', 'happy', 'false');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 15, 'M', 'black', 'angry', 'true');
