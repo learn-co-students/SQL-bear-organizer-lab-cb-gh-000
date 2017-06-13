35e7cac74c0cf3fa902146f80a98be5b91025016
Anurag Sharma <anurag30671371@gmail.com>
2017-06-13 10:22:56 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "select name,age from bears where gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "select name from bears order by name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "select name,age from bears where alive=1 order by age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "select name,age from bears order by age desc limit 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "select name,age from bears order by age limit 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "select color,count(color) from bears group by color order by count(color) desc limit 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "select count(*) from bears where temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "select * from bears where name is null;"
 end
