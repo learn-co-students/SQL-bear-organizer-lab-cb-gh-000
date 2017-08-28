521f3abc97e031da11857b2848a05376d4cb1ade
Melissa Sheehan <mxsheehan7@gmail.com>
2017-08-28 03:30:42 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+/*  id	INTEGER PRIMARY KEY, name	TEXT, age INTEGER, gender	TEXT, color TEXT, temperament TEXT, alive INTEGER)*/
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "smooth af", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "M", "brown", "rude", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1, "F", "brown", "smooth af", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 17, "M", "brown", "smooth af", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "F", "brown", "awesome", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 100, "M", "brown", "spirited", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "brown", "chicken", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "F", "brown", "smooth af", 1);
