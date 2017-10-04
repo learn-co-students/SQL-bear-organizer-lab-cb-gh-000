d2bb89a3a648803bd97f903fab47524ca2c22a51
Erwan Mailharro <erwan.mailharro@gmail.com>
2017-10-04 12:47:00 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id,name,age,gender,color,temperament,alive) values
+(1,"Mr. Chocolate", 5, "M","brown","nice",1),
+(2,"Rowdy",13,"M","black","angry",1),
+(3,"Tabitha",8,"F","black","calm",1),
+(4,"Sergeant Brown",6,"M","brown","calm",0),
+(5,"Melissa",4,"F","grey","calm",1),
+(6,"Grinch",15,"M","black","mean",1),
+(7,"Wendy",8,"F","white","weird",1),
+(8,NULL,6,"M","black","aggressive",1);
