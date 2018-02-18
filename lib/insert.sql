e3a15e450b54657a26719cbddce8f26844982835
Mars Wong <marswong618@gmail.com>
2018-02-18 09:33:49 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name) VALUES
+  (1, "Mr. Chocolate"),
+  (2, "Rowdy"),
+  (3, "Tabitha"),
+  (4, "Sergeant Brown"),
+  (5, "Melissa"),
+  (6, "Grinch"),
+  (7, "Wendy"),
+  (8, NULL);
