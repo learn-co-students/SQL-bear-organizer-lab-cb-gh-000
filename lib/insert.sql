ed541f4b2a8ad6ee612f0718d944f5e586453c29
norman lewis <takialog@gmail.com>
2018-03-05 01:27:38 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name) VALUES
+        ("Mr.Chocolate"),
+        ('Rowdy'),
+        ('Tabitha'),
+        ('Sergeant Brown'),
+        ('Melissa'),
+        ('Ginch'),
+        ('Wendy'),
+        (NULL);
