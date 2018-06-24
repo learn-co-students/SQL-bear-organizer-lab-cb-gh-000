d32e6ebb9f0c8c1d0bc00ef9da808abcbf6acca5
Philip Mottershead <philipmottershead@gmail.com>
2018-06-24 17:11:51 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -2,9 +2,8 @@ CREATE TABLE bears (
   id INTEGER PRIMARY key,
   name TEXT,
   age INTEGER,
-  gender INTEGER,
   gender CHAR(1),
-  color TEXT
+  color TEXT,
   temperament TEXT,
   alive BOOLEAN
 );
