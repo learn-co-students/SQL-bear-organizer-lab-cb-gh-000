33b17b8bcaf1f174ff0a698fad9b1ab5bc6cb19a
Philip Mottershead <philipmottershead@gmail.com>
2018-06-24 17:12:22 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,22 +1,22 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender='F'"
+  "SELECT name, age FROM bears WHERE gender='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name ASC"
+  "SELECT name FROM bears ORDER BY name ASC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive=1 ORDER BY age ASC"
+  "SELECT name, age FROM bears WHERE alive=1 ORDER BY age ASC;"
 
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name,MAX(age)FROM bears"
+  "SELECT name,MAX(age)FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name,MIN(age)FROM bears"
+  "SELECT name,MIN(age)FROM bears;"
 
 end
 
@@ -26,9 +26,10 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(*) FROM bears WHERE temperament ='goofy'"
+  "SELECT COUNT(*) FROM bears WHERE temperament ='goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE temperament = 'aggressive'"
+  "SELECT * FROM bears WHERE temperament = 'aggressive';"
 end
+x
