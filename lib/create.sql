faff6492ea984911dc68710c945847ee8b58beae
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-10 17:04:11 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER
+)
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) values (1, "Mr. Chocolate", 4, "M", "Green", "Shy" ,1);
+INSERT INTO bears VALUES (2, "Rowdy", 6, "M", "Brown", "Shy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 7, "F", "Gray", "Hyperactive", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "M", "Brown", "Temperamental", 1);
+INSERT INTO bears VALUES (5, "Melissa", 4, "F", "Brown", "Submissive", 1);
+INSERT INTO bears VALUES (6, "Grinch", 9, "M", "Brown", "Shy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 1, "F", "Gray", "Sharp-Shy", 1);
+INSERT INTO bears (age, gender, color, temperament, alive) values (9, "M", "Brown", "Temperamental", 0);
