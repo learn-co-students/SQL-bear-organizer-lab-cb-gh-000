6389dda85fbea58bf4dcd89b6df73d78cd8443f4
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-12-03 16:15:29 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr.Chocolate", 22, "M", "Blue", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 22, "F", "Blue", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitcha", 22, "M", "Blue", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 22, "M", "Blue", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 22, "M", "Blue", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 22, "M", "Blue", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 22, "M", "Blue", "Good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 22, "F", "Blue", "Good", 1);
