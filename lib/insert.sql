67c47ae8f90c5d7b845d50f7d015117d802a636d
Kaitlyn Wehrheim <iqy055@my.utsa.edu>
2018-12-31 05:39:54 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 2, 'Male', 'Brown', 'Docile', 0),
+('Rowdy', 4, 'Male', 'Red', 'Chaotic', 0),
+('Tabitha', 3, 'Female', 'Green', 'Docile', 0),
+('Sergeant Brown', 1, 'Male', 'Brown', 'Feisty', 0),
+('Melissa', 6, 'Female', 'Black', 'Reticent', 0),
+('Grinch', 11, 'Male', 'Black', 'Grouchy', 1),
+('Wendy', 2, 'Female', 'Brown', 'Docile', 0),
+(NULL, 3, 'Male', 'Brown', 'Chaotic', 1);
