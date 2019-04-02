ea2f9ce1f88f3f126eb053b2dc3d712a312ae335
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-04-02 16:52:40 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age) VALUES('Mr. Chocolate', 1);
+INSERT INTO bears(name) VALUES('Rowdy');
+INSERT INTO bears(name) VALUES('Tabitha');
+INSERT INTO bears(name) VALUES('Sergeant Brown');
+INSERT INTO bears(name) VALUES('Melissa');
+INSERT INTO bears(name) VALUES('Grinch');
+INSERT INTO bears(name) VALUES('Wendy');
+INSERT INTO bears(name, age) VALUES(NULL, 2);
